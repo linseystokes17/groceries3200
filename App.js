e701cabf8db501e191ddac8fdb034b21ca47c157
Linsey Stokes <linseystokes17@gmail.com>
2020-03-24 17:00:31 -0600

    init

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <SafeAreaView>
+        <Text> Hello world</Text>
+      </SafeAreaView>
+      );
+    }
+  }
