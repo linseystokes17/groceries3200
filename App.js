d95a98f140815dd2438e5ab155077f3793462cdd
Linsey Stokes <linseystokes17@gmail.com>
2020-11-05 18:50:14 -0600

    organizing

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {View, FlatList, Text, StyleSheet} from 'react-native';
 
 export default class App extends React.Component{
   render(){
     return(
-      <SafeAreaView>
-        <Text> Hello world</Text>
-      </SafeAreaView>
+      <View style = {styles.container}>
+        <FlatList data = {[{key: 'List1'}, {key: 'List2'}]} 
+        renderItem={({item}) => <Text style = {styles.item}>{item.key}</Text>}
+        />
+
+      </View>
       );
     }
   }
+
+  const styles = StyleSheet.create({
+    item:{
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+    },
+    container:{
+      flex: 1,
+      paddingTop: 22
+    }
+  })
